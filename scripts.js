bf13844110793a548d4a1b35929dd992d87877b5
laumujica <lauraamujica@gmail.com>
2024-06-04 18:29:23 -0300

    uploading project

---
+++ scripts.js
@@ -0,0 +1,13 @@
+// script.js
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const name = document.getElementById('name').value;
+    const phone = document.getElementById('phone').value;
+    const message = document.getElementById('message').value;
+
+    const whatsappNumber = "+5493512481479"; // Reemplaza con el número de WhatsApp asociado al QR
+    const url = `https://api.whatsapp.com/send?phone=${whatsappNumber}&text=${encodeURIComponent(`Nombre: ${name}\nTeléfono: ${phone}\nMensaje: ${message}`)}`;
+
+    window.open(url, '_blank');
+});
